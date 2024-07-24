f65c2f2c1f27a1c7e62ae5d4ea21e948ebd86de5
Abhishek Dubey <avdubey_b21@el.vjti.ac.in>
2024-07-24 22:42:02 +0530

    adding top navbar

--- client/src/scenes/navbar/index.tsx
+++ client/src/scenes/navbar/index.tsx
@@ -21,6 +21,32 @@ const Navbar = (props: Props) => {
                 Finanseer
             </Typography>
         </FlexBetween>
+
+        {/* {Right Side} */}
+        <FlexBetween gap="2rem">
+            <Box sx={{"&:hover":{color: palette.primary[100]}}}>
+                <Link to='/'
+                onClick={()=>setSelected('dashboard')}
+                style={{
+                    color:selected==='dashboard'? 'inherit':palette.grey[700],
+                    textDecoration:'inherit'
+                }}
+                >
+                    Dashboard
+                </Link>
+                </Box>
+                <Box sx={{"&:hover":{color: palette.primary[100]}}}>
+                <Link to='/predictions'
+                onClick={()=>setSelected('predictions')}
+                style={{
+                    color:selected==='predictions'? 'inherit':palette.grey[700],
+                    textDecoration:'inherit',
+                }}
+                >
+                    Predictions
+                </Link>
+            </Box>
+        </FlexBetween>
     
     </FlexBetween>
   
