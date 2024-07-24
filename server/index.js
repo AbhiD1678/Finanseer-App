95f30f4687efd70fde93c02791b17beeefb09bc0
Abhishek Dubey <avdubey_b21@el.vjti.ac.in>
2024-07-25 00:57:57 +0530

    adding configutation for api development/backend

--- server/index.js
+++ server/index.js
@@ -7,3 +7,16 @@ import dotenv from 'dotenv';
 import helmet from 'helmet';    
 import morgan from 'morgan';
 
+// Configurations
+
+dotenv.configu();
+const app=express();
+app.use(express.json);
+app.use(helmet);
+app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(cors());
+
+// Mongoose Setup
