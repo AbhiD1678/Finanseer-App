46dd9ee61f8e7bf1d93bc199a4e4aca31b786b2e
Abhishek Dubey <avdubey_b21@el.vjti.ac.in>
2024-07-25 11:56:16 +0530

    adding model schema

--- server/models/KPI.js
+++ server/models/KPI.js
@@ -5,13 +5,84 @@ const Schema=mongoose.Schema;
 
 loadType(mongoose);
 
+const daySchema=new Schema(
+    {
+        date:String,
+        revenue:{
+            type:moongoose.Types.Currency,
+            Currency:"INR",
+            get:(v)=>v/100
+        },
+        expenses:{
+            type:moongoose.Types.Currency,
+            Currency:"INR",
+            get:(v)=>v/100
+        },
+
+    },
+    {toJson:{getters:true}
+}
+
+)
+
+const monthSchema=new Schema(
+    {
+        month:String,
+        revenue:{
+            type:moongoose.Types.Currency,
+            Currency:"INR",
+            get:(v)=>v/100
+        },
+        expenses:{
+            type:moongoose.Types.Currency,
+            Currency:"INR",
+            get:(v)=>v/100
+        },
+        operationalExpenses:{
+            type:moongoose.Types.Currency,
+            Currency:"INR",
+            get:(v)=>v/100
+        },
+        nonOperationalExpenses:{
+            type:moongoose.Types.Currency,
+            Currency:"INR",
+            get:(v)=>v/100
+        },
+
+    },
+    {toJson:{getters:true}}
+)
 const KPISchema=new Schema({
     totalProfit:{
         type:moongoose.Types.Currency,
         Currency:"INR",
         get:(v)=>v/100
-    }
-})
+    },
+    totalRevenue:{
+        type:moongoose.Types.Currency,
+        Currency:"INR",
+        get:(v)=>v/100
+
+    },
+    totalExpenses:{
+    type:moongoose.Types.Currency,
+    Currency:"INR",
+    get:(v)=>v/100
+    },
+    expensesByCategory:{
+        type:Map,
+        of:{
+            type:moongoose.Types.Currency,
+            Currency:"INR",
+            get:(v)=>v/100
+
+        }
+    },
+    monthlyData:[monthSchema],
+    dailyData:[daySchema],
+},
+{timestamps:true,toJson:{getters:true}}
+)
 
 
 const KPI = mongoose.model("KPI",KPISchema);
